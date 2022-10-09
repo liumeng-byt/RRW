3f99171f483422dbb30a579d4f81040332b69d77
Administrator <yls_lm@163.com>
2022-10-09 17:58:22 +0800

    添加留言列表方法和excel测试文件

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 import pytest
 
-from testcase.test_rrw import test_login, test_like, test_friends
+from testcase.test_rrw import test_login, test_like, test_friends, test_message_list
 
 if __name__ == '__main__':
     # test_login()
     # test_like()
     # test_comment()
     # test_personal()
-    # test_friends()
+    # test_message_list()
     # pytest.main()
     pass
--- testcase/test_rrw.py
+++ testcase/test_rrw.py
@@ -4,31 +4,33 @@ from utils.assertutil import AssertUtil
 
 
 def test_login():
+    """
+    登录成功
+    :return:
+    """
     conf_read = ConfigYaml()
     base_url = conf_read.get_config_url()
-    # url2 = "https://rrwapi.renren.com/account/v1/loginByPassword"
     url = base_url + "/account/v1/loginByPassword"
     data = {
         "user": "15565280933",
         "password": "dc483e80a7a0bd9ef71d8cf973673924",
         "appKey": "bcceb522717c2c49f895b561fa913d10",
     }
-    headers = {
-        "Content-Type": "application/json",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"
-    }
     req_api = Requests()
-    response = req_api.post_api(url=url, json=data, headers=headers)
+    response = req_api.post_api(url=url, json=data, headers=None)
     print("登录---", response)
 
     err_assert = AssertUtil()
     err_assert.errorcode_assert(response['body']['errorCode'], 0)
 
-#
+
 def test_like():
+    """
+    点赞
+    :return:
+    """
     conf_read = ConfigYaml()
     base_url = conf_read.get_config_url()
-    # url = "https://rrwapi.renren.com/like/v1/send"
     url = base_url + "/like/v1/send"
     data = {
         "uid": 965194180,
@@ -54,35 +56,40 @@ def test_like():
     err_assert = AssertUtil()
     err_assert.errorcode_assert(response['body']['errorCode'], 0)
 
+
 def test_friends():
+    """
+    我的朋友列表
+    :return:
+    """
     conf_read = ConfigYaml()
     base_url = conf_read.get_config_person_url()
     # url = "http://www.renren.com/personal/965194180/friends"
     url = base_url + "/personal/965194180/friends"
-    # data = {
-    #
-    # }
     headers = {
         "Cookie": "Hm_lvt_ad6b0fd84f08dc70750c5ee6ba650172=1662869652; LOCAL_STORAGE_KEY_RENREN_USER_BASIC_INFO=%7B%22userName%22%3A%22%u674E%u5FB7%u5C71%22%2C%22userId%22%3A965194180%2C%22headUrl%22%3A%22http%3A//img.xiaonei.com/photos/0/0/men_head.gif%22%2C%22secretKey%22%3A%22abd9f37587aee6dd2cb45e2d04201891%22%2C%22sessionKey%22%3A%22betIJALLo2hDnkW0%22%7D; Hm_lpvt_ad6b0fd84f08dc70750c5ee6ba650172=1662876037",
     }
 
     req_api = Requests()
-    response = req_api.requests_api(url=url, headers=headers,method="get")
+    response = req_api.requests_api(url=url, headers=headers, method="get")
     print("我的朋友---", response)
     err_assert = AssertUtil()
     err_assert.body_include(response['body'], "李德山")
 
 
-def test_personal():
+def test_message_list():
+    """
+    我的留言
+    :return:
+    """
     conf_read = ConfigYaml()
-    base_url = conf_read.get_config_person_url()
-    # url="http://www.renren.com/personal/965194180"
-    url = base_url + "/personal/965194180"
-    headers = {
-        "Cookie": "Hm_lvt_ad6b0fd84f08dc70750c5ee6ba650172=1662869652; LOCAL_STORAGE_KEY_RENREN_USER_BASIC_INFO=%7B%22userName%22%3A%22%u674E%u5FB7%u5C71%22%2C%22userId%22%3A965194180%2C%22headUrl%22%3A%22http%3A//img.xiaonei.com/photos/0/0/men_head.gif%22%2C%22secretKey%22%3A%22abd9f37587aee6dd2cb45e2d04201891%22%2C%22sessionKey%22%3A%22betIJALLo2hDnkW0%22%7D; Hm_lpvt_ad6b0fd84f08dc70750c5ee6ba650172=1662876037",
-    }
+    base_url = conf_read.get_config_url()
+    url = base_url + "/messageboard/v1/getMessageList"
+    data = {"ownerId": 965194180, "limit": 10, "offset": 0, "appKey": "bcceb522717c2c49f895b561fa913d10",
+            "sessionKey": "betIJALLo2hDnkW0", "callId": "1665308617806", "sig": "6631c13a8188bab18ba268615b76cdb2"}
+
     req = Requests()
-    response = req.get_api(url=url, headers=headers)
-    print("个人主页---", response)
+    response = req.post_api(url=url, json=data, headers=None)
+    print("我的留言接口：", response)
     err_assert = AssertUtil()
-    err_assert.body_include(response['body'], "李德山")
+    err_assert.errorcode_assert(response['body']['errorCode'], 0)
