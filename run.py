cdd8fc6205146a75b751b341644222e37660909a
Administrator <yls_lm@163.com>
2022-10-19 10:31:59 +0800

    运行文件中添加“encoding=utf-8”

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+# encoding=utf-8
 import pytest
 from comman import base
 from config.conf import get_report_json_path, get_report_index_path
@@ -6,4 +7,4 @@ from config.conf import get_report_json_path, get_report_index_path
 
 if __name__ == '__main__':
     pytest.main()
-    base.allure_report(get_report_json_path(),get_report_index_path())  # 根据json文件生成html
+    base.allure_report(get_report_json_path(),get_report_index_path())  # 鏍规嵁json鏂囦欢鐢熸垚html
